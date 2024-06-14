b120e2d6756a1ad4279c2de90f768294ddd26cd5
XtratoS <28851891+XtratoS@users.noreply.github.com>
2024-06-15 01:30:31 +0300

    feat: Add form validation and error handling to invoice creation

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -5,18 +5,31 @@ import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import { z } from 'zod';
 
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+}
+
 const invoiceFormSchema = z.object({
   id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(['pending', 'paid']),
+  customerId: z.string({
+    invalid_type_error: 'Please select a customer.',
+  }),
+  amount: z.coerce.number().gt(0, { message: 'Please enter an amount greater than $0.' }),
+  status: z.enum(['pending', 'paid'], {
+    invalid_type_error: 'Please select an invoice status.',
+  }),
   date: z.string(),
 });
 
 const CreateInvoice = invoiceFormSchema.omit({ id: true });
 const UpdateInvoice = invoiceFormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData: FormData) {
+export async function createInvoice(prevState: State, formData: FormData) {
   const customerId = formData.get('customerId');
   const status = formData.get('status');
 
@@ -24,17 +37,24 @@ export async function createInvoice(formData: FormData) {
   const numberAmount = Number(amount);
   const centAmount = numberAmount * 100;
   
-  const validatedData = CreateInvoice.parse({
+  const validatedData = CreateInvoice.safeParse({
     customerId,
     amount: centAmount,
     status,
     date: new Date().toISOString().split('T')[0],
   });
 
+  if (!validatedData.success) {
+    return {
+      errors: validatedData.error.flatten().fieldErrors,
+      message: 'Missing Fields or Invalid Data. Failed to Create Invoice'
+    };
+  }
+
   try {
     await sql`
       INSERT INTO invoices (customer_id, amount, status, date)
-      VALUES (${validatedData.customerId}, ${validatedData.amount}, ${validatedData.status}, ${validatedData.date})
+      VALUES (${validatedData.data.customerId}, ${validatedData.data.amount}, ${validatedData.data.status}, ${validatedData.data.date})
     `;
   } catch (error) {
     return { message: 'Database Error: Failed to Create Invoice' };
